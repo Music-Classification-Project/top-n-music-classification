162f3c1a097099efa93b89faf2b071adcd736a01
Jaclyn Rutter <jaclynrutter19@gmail.com>
2025-10-26 14:00:15 -0400

    Raw stats added

---
+++ src/data_analysis/normalized_dataset_analysis.py
@@ -0,0 +1,4 @@
+"""
+Analysis of NORMALIZED GTZAN datset.
+
+"""
--- src/data_analysis/original_dataset_analysis.py
+++ src/data_analysis/original_dataset_analysis.py
@@ -1,4 +1,7 @@
 import os, os.path
+from scipy.io import wavfile
+
+import numpy as np
 
 """
 Analysis of raw GTZAN datset.
@@ -37,6 +40,47 @@ def return_genre_lst():
 
     return genres_downloaded
 
+def return_wave_statistics():
+    """
+    Input: Unaldertered wave file from raw dataset
+    Output: Wave statistics returned.
+    """
+
+    wav_stats = []
+    wav_stats, samplerates, durations, mean_amplitudes = [], [], [], []
+
+    for root, dirnames, filenames in os.walk(RAW_GENRE_PATH):
+        for filename in filenames:
+            try:
+                samplerate, data = wavfile.read(root+ "/" + filename)
+                duration_sec = len(data)/samplerate
+                mean_amplitude = np.mean(data)
+                stats = {
+                    'filename': filename,
+                    'samplerate': samplerate,
+                    'duration_sec': duration_sec,
+                    'mean_amplitude': mean_amplitude
+                }
+
+                wav_stats.append(stats)
+                samplerates.append(samplerate)
+                durations.append(duration_sec)
+                mean_amplitudes.append(mean_amplitude)
+
+            except Exception as e:
+                print(f'Error processing {filename}: {e}')
+
+    return {'mean_duration': int(np.mean(durations)),
+            'mean_amplitude': int(np.mean(mean_amplitudes)),
+            'mean_samplerate': int(np.mean(samplerates)),
+            'min_duration': int(np.min(durations)),
+            'max_duration': int(np.max(durations)),
+            'min_amplitude': int(np.min(mean_amplitudes)),
+            'max_amplitude': int(np.max(mean_amplitudes)),
+            'min_samplerate': int(np.min(samplerates)),
+            'max_samplerate': int(np.max(samplerates)),}
+
+
 def gtzan_raw_analysis():
     print("GTZAN Raw Analysis\n")
 
@@ -47,7 +91,10 @@ def gtzan_raw_analysis():
     genres_downloaded = return_genre_lst()
     [print(f'{genre} {len(os.listdir(RAW_GENRE_PATH + genre))}') for genre in genres_downloaded]
     if genres_downloaded.sort() == expected_genres.sort():
-        print("\nDataset uploaded as expected!")
+        print("\nDataset genres match expected.\n")
+
+    stats = return_wave_statistics()
+    [print(f'{stat}: {stats[stat]}') for stat in stats]
 
 if __name__ == "__main__":
     gtzan_raw_analysis()
