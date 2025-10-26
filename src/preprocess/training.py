4a70ea2dad0e493ab81d6f819e61ce8e22a5b12d
Camden Warncke <warnckcj@mail.uc.edu>
2025-10-26 13:17:22 -0600

    Added training.py. Still need to fix some bugs in it

---
+++ src/preprocess/training.py
@@ -0,0 +1,98 @@
+import numpy as np
+import json
+import os
+from model_cnn import build_baseline_cnn_model
+from keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, TensorBoard
+
+
+BASE_PATH = os.path.abspath('../../data/features/Data')
+print('XXXXXXXXXXXXXXXXXXXXX')
+print(os.getcwd())
+
+#Genres aren't part of .npz files, so I used metadata.json to build a dictionary to map filename to genre
+#Load metadata
+metadata_path = os.path.join(BASE_PATH, 'metadata.json')
+with open(metadata_path, 'r') as f:
+    metadata = json.load(f)
+
+#Dictionary to associate genre with filename
+filename_to_genre = {entry['filename']: entry['genre'] for entry in metadata}
+
+#Build label map for neural net
+genres = sorted(list(set(filename_to_genre.values())))
+label_map = {genre: i for i, genre in enumerate(genres)}
+
+#Build lists of filepaths and labels for neural net
+file_paths = []
+labels = []
+for filename, genre in filename_to_genre.items():
+    full_path = os.path.join(BASE_PATH,'*',filename)
+    if os.path.exists(full_path):
+        file_paths.append(full_path)
+        labels.append(label_map[genre])
+    else:
+        print(f'Missing file: {full_path}')
+
+
+class DataGenerator:
+    """Generates batches of training data, so all files don't have to be loaded into RAM at once"""
+    def __init__(self, file_paths, labels, batch_size, num_classes, shuffle=True):
+        self.file_paths = file_paths
+        self.labels = labels
+        self.batch_size = batch_size
+        self.num_classes = num_classes
+        self.shuffle = shuffle
+        self.indices = np.arange(len(file_paths))
+
+    def __len__(self):
+        #Number of batches per epoch
+        return int(np.ceil(len(self.file_paths) / self.batch_size))
+    
+    def __iter__(self):
+        while True:
+            if self.shuffle:
+                np.random.shuffle(self.indices)
+            for i in range(0, len(self.file_paths), self.batch_size):
+                batch_indices = self.indices[i:i+self.batch_size]
+                x_batch = []
+                y_batch = []
+                for j in batch_indices:
+                    data = np.load(self.file_paths[j])
+                    x_batch.append(data['mel_spec'])
+                    y_batch.append(self.labels[j])
+                x_batch = np.array(x_batch)
+                y_batch = to_categorical(y_batch, num_classes=self.num_classes)
+
+                yield x_batch, y_batch
+
+
+#Split data into training and testing sets
+train_paths, test_paths, train_labels, test_labels = train_test_split(file_paths, labels, test_size=0.2, stratify=labels, random_state=27)
+
+#Build generators
+batch_size = 10
+train_gen = DataGenerator(train_paths, train_labels, batch_size=batch_size, num_classes=len(label_map))
+test_gen = DataGenerator(test_paths, test_labels, batch_size=batch_size, num_classes=len(label_map), shuffle=False)
+
+#Build model
+model = build_baseline_cnn_model()
+
+#Callbacks
+callbacks = [
+    EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True),
+    TensorBoard(log_dir='logs', histogram_freq=1)
+]
+
+#Train model
+model.fit(
+    train_gen,
+    validation_data=test_gen,
+    epochs=30,
+    steps_per_epoch=len(train_paths)//batch_size,
+    validation_steps=len(test_paths)//batch_size
+)
+
+#Save model
+model.save('music_genre_classifier.keras')
