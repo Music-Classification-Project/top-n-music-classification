aeb52958ffa970984664d6392e117ab5045aa451
Jonas Field-Patton <jfieldpatton@gmail.com>
2025-11-05 10:14:18 +1030

    Add zero-centered normalization of features

--- src/preprocess/extract_features.py
+++ src/preprocess/extract_features.py
@@ -33,7 +33,9 @@ def extract_features_from_file(file_path: str,
                                     n_mfcc=config["n_mfcc"],
                                     hop_length=config["hop_length"])
 
-        features["mfcc"] = np.expand_dims(mfcc, axis=-1)
+        mfcc_norm = (mfcc - np.mean(mfcc)) / (np.std(mfcc) + 1e-6)
+
+        features["mfcc"] = np.expand_dims(mfcc_norm, axis=-1)
 
     if config["use_mel"]:
         mel_spec = librosa.feature.melspectrogram(
@@ -42,20 +44,26 @@ def extract_features_from_file(file_path: str,
             n_fft=config["n_fft"],
             hop_length=config["hop_length"],
             n_mels=config["n_mels"]
-            )
+        )
 
         # Convert to decibels for better numerical stability
         mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
 
-        features["mel_spec"] = np.expand_dims(mel_spec_db, axis=-1)
+        # Zero-centered normalization
+        mel_spec_db_norm = (mel_spec_db - np.mean(mel_spec_db)
+                            ) / (np.std(mel_spec_db) + 1e-6)
+
+        features["mel_spec"] = np.expand_dims(mel_spec_db_norm, axis=-1)
 
     if config["use_chroma"]:
         chroma = librosa.feature.chroma_stft(y=audio_array, sr=sample_rate,
                                              n_fft=config["n_fft"],
                                              hop_length=config[
-                                              "hop_length"])
+                                                 "hop_length"])
 
-        features["chroma"] = np.expand_dims(chroma, axis=-1)
+        chroma_norm = (chroma - np.mean(chroma)) / (np.std(chroma) + 1e-6)
+
+        features["chroma"] = np.expand_dims(chroma_norm, axis=-1)
 
     return features
 
