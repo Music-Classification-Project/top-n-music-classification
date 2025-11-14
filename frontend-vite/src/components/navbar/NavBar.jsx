7c54fcb4767423f849e38a706e3a3bc9ffa41bc1
Jaclyn Rutter <jaclynrutter19@gmail.com>
2025-11-13 20:50:27 -0500

    navbar added

---
+++ frontend-vite/src/components/navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+
+        <nav className="navbar">
+            <div className="navbar-left">
+                <a href="/" className="logo">
+                    ShopNow
+                </a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar
