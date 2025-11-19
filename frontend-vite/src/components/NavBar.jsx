d4dbb40b93d18bd09fd1b3d3b370054f2ad57db1
Jaclyn Rutter <jaclynrutter19@gmail.com>
2025-11-19 11:35:02 -0500

    header updated

--- frontend-vite/src/components/NavBar.jsx
+++ frontend-vite/src/components/NavBar.jsx
@@ -12,16 +12,16 @@ const Navbar = () => {
             <div class=" flex flex-wrap w-full items-center justify-between mx-auto p-3">
                 <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img class="h-7" src={soundWave} alt="audio wave"/>
-                    <span class="self-center text-2xl text-heading font-semibold whitespace-nowrap">Top-N</span>
+                    <span class="self-center text-xl text-heading font-semibold whitespace-nowrap">Top-N</span>
                 </a>
-                <div class="font-semibold font-display text-lg">
+                <div class="font-semibold font-display">
                     <a class="p-4">Team</a>
                     <a class="p-4">About</a>
                     <a class="p-4">Contact</a>
                 </div>
                 <button 
                 type="button" 
-                class="text-white text-base bg-darkestgreen box-border border border-transparent hover:bg-success-strong focus:ring-4 focus:success-subtle shadow-xs font-medium leading-5 rounded-full px-4 py-2.5 focus:outline-none">
+                class="text-white bg-darkestgreen box-border border border-transparent hover:bg-success-strong focus:ring-4 focus:success-subtle shadow-xs font-medium leading-5 rounded-full px-4 py-2.5 focus:outline-none">
                 Learn More</button>
             </div>
         </nav>
