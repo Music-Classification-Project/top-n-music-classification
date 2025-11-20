2b3858475af2aa0638b9e28e70c72a86ff6e90e8
Jonas Field-Patton <jfieldpatton@gmail.com>
2025-11-20 21:24:11 +1030

    Format to flake8 standards

--- sandbox/jonas/cnn_model/get_mel_shape.py
+++ sandbox/jonas/cnn_model/get_mel_shape.py
@@ -9,7 +9,8 @@ import os
 root = "./data/features/gtzan/train"
 for genre in os.listdir(root):
     gdir = os.path.join(root, genre)
-    if not os.path.isdir(gdir): continue
+    if not os.path.isdir(gdir):
+        continue
     f = next(x for x in os.listdir(gdir) if x.endswith(".npz"))
     path = os.path.join(gdir, f)
     with np.load(path) as d:
