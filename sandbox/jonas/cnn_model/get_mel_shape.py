55d07e82b37844279e39d867b8ce95a583e6e4d9
Jonas Field-Patton <jfieldpatton@gmail.com>
2025-11-20 21:22:27 +1030

    Add file used to check shape of features

---
+++ sandbox/jonas/cnn_model/get_mel_shape.py
@@ -0,0 +1,16 @@
+import numpy as np
+import os
+
+# with np.load("sandbox/jonas/cnn_model/blues.00000_win0.npz") as d:
+#     x = d["mel_spec"]
+#     print(x.min(), x.max(), x.mean(), x.std())
+
+
+root = "./data/features/gtzan/train"
+for genre in os.listdir(root):
+    gdir = os.path.join(root, genre)
+    if not os.path.isdir(gdir): continue
+    f = next(x for x in os.listdir(gdir) if x.endswith(".npz"))
+    path = os.path.join(gdir, f)
+    with np.load(path) as d:
+        print(path, d["mel_spec"].shape)
